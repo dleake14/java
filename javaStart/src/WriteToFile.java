3dc916b68cf706a7a23820a6a5e5c1fca4a485cb
David Leake <dleake14@gmail.com>
2019-03-11 15:11:39 -0500

    0311 Changes 3:11pm

--- javaStart/src/WriteToFile.java
+++ javaStart/src/WriteToFile.java
@@ -5,19 +5,25 @@ import java.io.File;
 import java.util.Random;
 
 public class WriteToFile
-{	public static void main(String[] args)
-{	try
-{	PrintStream writer = new PrintStream( new File("randInts.txt"));
-    Random r = new Random();
-    final int LIMIT = 100;
-
-    for(int i = 0; i < LIMIT; i++)
-    {	writer.println( r.nextInt() );
+    {
+        public static void main(String[] args)
+        {
+            try
+            {
+                PrintStream writer = new PrintStream( new File("randInts.txt"));
+                Random r = new Random();
+                final int LIMIT = 100;
+                for(int i = 0; i < LIMIT; i++)
+            {
+            writer.println( r.nextInt() );
+        }
+        writer.close();
     }
-    writer.close();
-}
 catch(IOException e)
-{	System.out.println("An error occured while trying to write to the file");
-}
-}
-}
+    {
+    System.out.println("An error occured while trying to write to the file");
+
+    }
+        }
+
+    }
