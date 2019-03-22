c3b915fb6ebb4b764a4fd65a3c93daa02da043f7
David Leake <dleake14@gmail.com>
2019-03-21 20:17:21 -0500

    Java init for first project

---
+++ java-one/src/com/example/basic.java
@@ -0,0 +1,8 @@
+package com.example;
+
+import javax.swing.*;
+
+public class basic {
+    private JTextArea TESTINGGUIOUTPUTTextArea;
+    private JPanel panel1;
+}
---
+++ java-one/src/com/example/main.java
@@ -0,0 +1,11 @@
+package com.example;
+
+
+public class main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
+
